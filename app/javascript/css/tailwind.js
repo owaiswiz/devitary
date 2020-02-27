b7625cd58e0904d2de9cb60f353d6043672ecec8
Owais <owaiswiz@gmail.com>
2020-02-27 20:47:48 +0530

    Add home page and styles

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -4,6 +4,16 @@ module.exports = {
       fontFamily: {
         'display': ["Inter", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
       },
+      borderRadius: {
+        'xl': "1rem",
+        '2xl': "2rem"
+      },
+      opacity: {
+        '65': '65%'
+      },
+      zIndex: {
+        '-10': '-10'
+      }
     },
   },
   variants: {},
