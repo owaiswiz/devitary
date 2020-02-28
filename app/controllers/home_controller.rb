490d7c4bdbbf09f65006fdc7d0abf520c58189ad
Owais <owaiswiz@gmail.com>
2020-02-28 15:38:28 +0530

    Store directly in tmp dir

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     begin
       file = params[:file]
       file_name = "#{Time.now.to_i}#{rand(9000000000)}-#{file.original_filename}"
-      file_path = Rails.root.join("tmp", "storage", file_name)
+      file_path = Rails.root.join("tmp", file_name)
 
       File.open(file_path, "wb") {|f| f.write(file.read)}
       uploaded_url = helpers.upload_file_to_firebase file_path
